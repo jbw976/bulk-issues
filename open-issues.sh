8a4cc91eacb3f5b2035843819c5b1edd508d7b7b
Jared Watts <jbw976@gmail.com>
2025-02-28 10:11:57 -0800

    slow down a bit to avoid rate limiting
    Signed-off-by: Jared Watts <jbw976@gmail.com>

--- open-issues.sh
+++ open-issues.sh
@@ -5,4 +5,5 @@ issue_title="Compliance: Community Extension Projects"
 # Loop through the repositories in repos.txt and create an issue in each
 while IFS= read -r repo || [ -n "$repo" ]; do
   gh issue create --repo "$repo" --title "$issue_title" --body-file body.md
+  sleep 3  # slow down a bit to avoid rate limiting
 done < repos.txt
